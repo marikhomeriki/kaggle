96cbb0db3d325a2b6fc91a0be2a901898680c17d
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-09-13 14:15:46 +0400

    testing

--- blackjack.py
+++ blackjack.py
@@ -9,3 +9,16 @@ def hand_total(hand):
             aces += 1
         else:
             total += int(card)
+
+    total += aces
+
+    while total + 10 <= 21 and aces > 0:
+        print(total)
+        total += 10
+        aces += -1
+    return total
+
+
+hand = 'A', 'A', 'J', '6'
+score = hand_total(hand)
+print(score)
