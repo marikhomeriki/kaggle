f853d1b23d95af052fbcf9afa9b6e612fdd9f990
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-10-04 00:46:31 +0100

    start wave

---
+++ wave.py
@@ -0,0 +1,16 @@
+def wave(people):
+    result = []
+    word = ''
+    size = len(people)
+    for i in range(size):
+
+        c = people[i].upper()
+        word = word + c + people[i+1:]
+        result.append(word)
+        word = word[0:i+1].lower()
+    print(result)
+
+
+people = "Two words"
+
+wave(people)
