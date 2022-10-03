564a973f948d5b910e4f3dbb3c0db88e7aaab676
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-10-03 22:32:35 +0100

    colorful

--- iscolorful.py
+++ iscolorful.py
@@ -1,2 +1,14 @@
 def is_colorful(num):
-    pass
+    digits = [int(x) for x in str(num)]
+    length = len(digits)
+
+    if length == 1:
+        return True
+
+    for i in range(length-1):
+        digits.append(digits[i]*digits[i+1])
+    return len(set(digits)) == len(digits)
+
+
+num = 236
+is_colorful(num)
