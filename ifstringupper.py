cdfb2784566fd9499d123e40beb4191656d0e02a
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-10-03 22:43:07 +0100

    is string upper

---
+++ ifstringupper.py
@@ -0,0 +1,14 @@
+def is_uppercase(inp):
+    return inp.upper() == inp
+
+
+a = "MARI"
+b = "mari"
+print(a.upper())
+print(a == b)
+
+inp = "$&$"
+inp_up = inp.upper()
+print(inp_up)
+
+print(is_uppercase(inp))
