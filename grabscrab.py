37b7602e4d552ca4276478e16f507da40481aaa8
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-10-04 19:21:28 +0100

    init grab scrab

---
+++ grabscrab.py
@@ -0,0 +1,11 @@
+def grabscrab(word, possible_words):
+    for c in word:
+        print('test')
+
+    return []
+
+
+word = "ortsp",
+print(word.count('o'))
+possible_words = ["sport", "parrot", "ports", "matey"]
+grabscrab(word, possible_words)
