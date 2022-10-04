0eb5345bade72b9d4225a54faffabde524bf996f
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-10-04 21:57:43 +0100

    first v of roman to int

---
+++ romannumerals.py
@@ -0,0 +1,24 @@
+def roman_to_int(roman):
+    ROMANS = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    result = 0
+
+    for i in range(len(roman)):
+        if i > 0 and ROMANS[roman[i]] > ROMANS[roman[i-1]]:
+            result += ROMANS[roman[i]] - 2*ROMANS[roman[i-1]]
+        else:
+            result += ROMANS[roman[i]]
+
+    return result
+
+
+roman = 'XLVIII'
+
+print(roman_to_int(roman))
