1264a203d794e4137f2157c6b5eb11828acbb8a3
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-10-04 19:10:54 +0100

    finish find it

--- findit.py
+++ findit.py
@@ -1,5 +1,12 @@
 def find_it(seq):
-    pass
+    for i in range(len(seq)):
+        x = seq.count(seq[i])
+        if x % 2 == 1:
+            return seq[i]
 
 
-seq = [0, 1, 0, 1, 0]
+seq = [0, 1, 0, 1, 0]  # 1
+seq1 = [1, 1, 2]  # 2
+seq2 = [7]
+
+print(find_it(seq2))
