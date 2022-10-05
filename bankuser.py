fc62f1b365dbbc3c37af954a4e815bcde3430676
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-10-05 14:45:18 +0100

    add all methods

--- bankuser.py
+++ bankuser.py
@@ -6,12 +6,28 @@ class User(object):
 
     def withdraw(self, amount):
         if self.balance > amount:
-            balance = self.balance - amount
-            return f"Jeff has {balance}."
+            self.balance = self.balance - amount
+            return f"{self.name} has {self.balance}."
         else:
             raise ValueError("No sufficient balance")
 
+    def add_cash(self, amount):
+
+        self.balance = self.balance + amount
+        return f"{self.name} has {self.balance}."
+
+    def check(self, other, amount):
+        if not other.checking_account:
+            raise ValueError()
+        other.withdraw(amount)
+        self.add_cash(amount)
+        return f"{self.name} has {self.balance} and {other.name} has {other.balance}."
+
 
 Jeff = User('Jeff', 70, True)
+Joe = User('Joe', 70, True)
 
-print(Jeff.withdraw(71))
+print(Jeff.withdraw(2))
+print(Joe.check(Jeff, 50))
+print(Jeff.check(Joe, 80))
+print(Jeff.add_cash(20))
